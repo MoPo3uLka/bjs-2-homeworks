9c393b264ad79a54c657cf5a48e144d22b706cc5
NetoDen <timonindanil664@gmail.com>
2025-05-25 14:30:27 +0700

    Иправлено задание1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,37 +2,26 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-	let root1, root2;
 	let discriminant = b ** 2 - 4 * a * c;
 
 	if (discriminant > 0) {
-		root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-		root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 		arr.push(root1, root2);
-
 	} else if (discriminant === 0) {
-		root1 = -b / (2 * a);
+		let root1 = -b / (2 * a);
 		arr.push(root1);
-
 	}
 
 	return arr;
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	percent = parseInt(percent) / 100 / 12;
+	let loanBody = amount - contribution;
+	let pay = loanBody * (percent + percent / ((1 + percent) ** countMonths - 1));
+	let totalAmount = (pay * countMonths).toFixed(2);
 
-	let p = parseInt(percent) / 100 / 12;
-	if (isNaN(p) || p < 0) {
-		return false;
-	} else if (isNaN(contribution) || contribution < 0) {
-		return false;
-	} else if (isNaN(amount) || amount < 0) {
-		return false;
-	} else {
-		let s = amount - contribution;
-		let n = countMonths;
-		let pay = s * (p + p / ((1 + p) ** n - 1));
-		let totalAmount = (pay * n).toFixed(2);
-		return Number(totalAmount);
-	}
+	return Number(totalAmount);
 }
