ca8c410b450f262a69b41a6ffb0e00bb9c17fefd
NetoDen <timonindanil664@gmail.com>
2025-05-25 15:15:04 +0700

    Переменные убраны

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,12 +5,9 @@ function solveEquation(a, b, c) {
 	let discriminant = b ** 2 - 4 * a * c;
 
 	if (discriminant > 0) {
-		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-		arr.push(root1, root2);
+		arr.push(((-b + Math.sqrt(discriminant)) / (2 * a)), ((-b - Math.sqrt(discriminant)) / (2 * a)));
 	} else if (discriminant === 0) {
-		let root1 = -b / (2 * a);
-		arr.push(root1);
+		arr.push(-b / (2 * a));
 	}
 
 	return arr;
