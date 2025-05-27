a0164447fd8a07f2078ea90c1f190329402283b2
NetoDen <timonindanil664@gmail.com>
2025-05-27 16:38:20 +0700

    Задание сделано

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,41 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (typeof this.marks === false || this.marks === undefined) {
+		return 0;
+	}
+
+	this.marks = [...this.marks, ...marks];
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (typeof this.marks === false || this.marks === undefined) {
+		return 0;
+	}
+
+	let result = this.marks.reduce((acc, item, index, arr) => {
+		acc += item;
+		if (index === arr.length - 1) {
+			return acc / arr.length
+		}
+		return acc;
+	}, 0);
+
+	return result;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+
+	this.excluded = reason;
 }
