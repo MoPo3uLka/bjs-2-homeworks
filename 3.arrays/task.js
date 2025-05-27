6970d2964a315b039e0a42b7ee236a818e6e10a8
NetoDen <timonindanil664@gmail.com>
2025-05-27 15:55:19 +0700

    Задание сделано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((items, index) => items === arr2[index]);   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter((element) => {
+        return element.gender === gender;
+    }).map(element => element.age).reduce((acc, item, index, arr) => {
+        const sum = acc + item;
+        if (index === arr.length - 1) {
+        return sum / arr.length;
+        }
+        return sum
+    }, 0)
+    return result;
 }
