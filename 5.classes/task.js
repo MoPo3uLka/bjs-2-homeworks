4267a722e9a7019fffebecb1ab4414c401c5105e
NetoDen <timonindanil664@gmail.com>
2025-05-29 18:36:12 +0700

    Задание сделано

---
+++ 5.classes/task.js
@@ -0,0 +1,87 @@
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		return this.state *= 1.5;
+	}
+
+	set state(value) {
+		if (value < 0) {
+			this._state = 0;
+		} else if (value > 100) {
+			this._state = 100;
+		} else this._state = value;
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = 'book';
+		this.author = author;
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'novel';
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'fantastic';
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = 'detective';
+	}
+}
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			return this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+		return this.books.find(book => book[type] === value) || null;
+	}
+
+	giveBookByName(bookName) {
+		let givingBook = this.books.find(book => book.name === bookName);
+
+		if (givingBook !== undefined) {
+			this.books = this.books.filter(book => book.name !== bookName);
+			return givingBook;
+		} else return null;
+	}
+}
